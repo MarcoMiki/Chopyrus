6ecd5ac6a6afe05a64577638f98af8d9fea7502b
Marco <marco.michelutto@gmai.com>
2021-10-01 13:45:40 +0300

    some refactoring, also added descriptions to all function so far

--- Chopyrus.py
+++ Chopyrus.py
@@ -13,17 +13,17 @@ class Chorus:
     # FUNCTIONS TO GET USER DATA AND EXPORT IT
 
     def get_user_ids(self):
-        """function"""
+        """returns the IDs of all site users"""
         response = requests.get(url=self.url + "/rest/v1/site/users", headers=self.headers)
         return response.json()["response"]
 
     def get_user_details(self, user_id):
-        """function"""
+        """given a user id, returns details for that user"""
         response = requests.get(url=self.url + f"/rest/v1/users/{user_id}", headers=self.headers)
         return response.json()
 
     def get_multiple_user_details(self):
-        """function"""
+        """given a list of ids, returns details for them"""
         ids_list = self.get_user_ids()
         body = {
             "userIds": ids_list
@@ -32,7 +32,7 @@ class Chorus:
         return response.json()
 
     def export_user_data(self, path):
-        """function"""
+        """given a path to export to, downloads a .csv file with details for all site's users"""
         user_details = self.get_multiple_user_details()
         data = user_details["response"]
         df = pandas.DataFrame(data)
@@ -41,7 +41,8 @@ class Chorus:
     # FUNCTIONS TO CREATE USERS
 
     def create_site_user(self, name, email, username, description, home_shortcut):
-        """function"""
+        """given name, email, username, description and True/False on whether the home shortcut should be displayed,
+        it creates a user"""
         new_user = {
             "description": description,
             "email": email,
@@ -52,7 +53,8 @@ class Chorus:
         requests.post(url=self.url + "/rest/v1/site/users", json=new_user, headers=self.headers)
 
     def create_space_user(self, space_id, name, email, username, description, home_shortcut):
-        """function"""
+        """given name, email, username, space ID, description and True/False on whether the home shortcut should be
+        displayed, it creates a user in a specific space"""
         new_user = {
             "description": description,
             "email": email,
@@ -63,7 +65,9 @@ class Chorus:
         requests.post(url=self.url + f"/rest/v1/spaces/{space_id}/users", json=new_user, headers=self.headers)
 
     def create_multiple_space_users(self, path):
-        """function"""
+        """given the path to a .csv spreadsheet, it creates multiple users in a specific space from the details within
+        it. The spreadsheet needs to contain the following headers: name, username, email, description, space_id,
+        home_shortcut (boolean value)"""
         user_data = pandas.read_csv(path)
         users = pandas.DataFrame.to_dict(user_data, orient="records")
         for entry in users:
@@ -71,12 +75,14 @@ class Chorus:
             username = entry["username"]
             email = entry["email"]
             description = entry["description"]
+            home_shortcut= entry["home_shortcut"]
             space_id = entry["space_id"]
             self.create_space_user(space_id=space_id, name=name, email=email, username=username,
-                                   description=description, home_shortcut=True)
+                                   description=description, home_shortcut=home_shortcut)
 
     def create_multiple_site_users(self, path):
-        """function"""
+        """given the path to a .csv spreadsheet, it creates multiple users from the details within it. The spreadsheet
+        needs to contain the following headers: name, username, email, description, home_shortcut (boolean value)"""
         user_data = pandas.read_csv(path)
         users = pandas.DataFrame.to_dict(user_data, orient="records")
         for entry in users:
@@ -84,17 +90,19 @@ class Chorus:
             username = entry["username"]
             email = entry["email"]
             description = entry["description"]
+            home_shortcut = entry["home_shortcut"]
             self.create_site_user(name=name, email=email, username=username, description=description,
-                                  home_shortcut=True)
+                                  home_shortcut=home_shortcut)
 
     # FUNCTIONS TO DELETE USERS
 
     def delete_user(self, user_id):
-        """function"""
+        """given a user id, it deletes that user"""
         requests.delete(url=self.url + f"/rest/v1/users/{user_id}", headers=self.headers)
 
     def delete_multiple_users(self, path):
-        """function"""
+        """given a path to a .csv file containing multiple user ids, it deletes those users. The column header name for
+        the id values needs to be id"""
         data = pandas.read_csv(path)
         ids_df = pandas.DataFrame(data)
         ids_list = ids_df["id"].tolist()
@@ -104,14 +112,16 @@ class Chorus:
     # FUNCTIONS TO MODIFY USERS
 
     def patch_user(self, user_id, value_type, value):
-        """function"""
+        """given a user id, value type and a new value it modifies that value for that user. Value types available are
+        name, email or description"""
         patch = {
             value_type: value
         }
         requests.patch(url=self.url + f"/rest/v1/users/{user_id}", json=patch, headers=self.headers)
 
     def patch_multiple_users(self, path, value_type):
-        """function"""
+        """given a value type and a .csv file containing user ids and new values, it modifies those values for those
+        users. Value types available are name, email or description"""
         data = pandas.read_csv(path)
         df = pandas.DataFrame(data)
         ids_list = df["id"].tolist()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from Chorus import Chorus
+import os
+from Chopyrus import Chorus
 
-key = ...
-url = ...
+key = os.getenv("API_KEY")
+url = os.getenv("URL")
 path_to_desktop = "~/desktop/csv.csv"
 
 chorus = Chorus(key=key, url=url)
 
-
+chorus.create_multiple_site_users("~/desktop/create.csv")
 
