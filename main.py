121a4fc16cc93503fa661dd394caad8ab7884cbc
Marco <marco.michelutto@gmai.com>
2021-10-04 16:44:45 +0300

    cleaned up main

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ path_to_desktop = "~/desktop/csv.csv"
 
 chorus = Chorus(key=key, url=url)
 
-chorus.create_multiple_site_users("~/desktop/create.csv")
-
+# print(chorus.get_multiple_file_ids('56af1aaa-396e-4aa8-9d71-f5cde39b1751'))
+print(chorus.get_file_metadata(file_id='5ae23222-3306-4da7-8779-d98b73e39924'))
