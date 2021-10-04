ca7e1c9b8a325928b0a6e02f36f25b66f5ef0c80
Marco <marco.michelutto@gmai.com>
2021-10-04 16:45:08 +0300

    cleaned up main

--- main.py
+++ main.py
@@ -7,5 +7,4 @@ path_to_desktop = "~/desktop/csv.csv"
 
 chorus = Chorus(key=key, url=url)
 
-# print(chorus.get_multiple_file_ids('56af1aaa-396e-4aa8-9d71-f5cde39b1751'))
-print(chorus.get_file_metadata(file_id='5ae23222-3306-4da7-8779-d98b73e39924'))
+# test methods here
